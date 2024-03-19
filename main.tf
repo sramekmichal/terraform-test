046bdcb71e170bf29d9d42513be8dd1a4fa991d6
Michal Šrámek <115155195+sramekmichal@users.noreply.github.com>
2024-03-19 17:02:47 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region  = "eu-west-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
